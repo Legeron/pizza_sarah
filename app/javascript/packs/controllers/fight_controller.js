a8b291e9ff3a378186d53b8e4e7e71b55a7a6881
Florent Legeron <legeronflo@hotmail.fr>
2019-04-17 17:15:16 +0200

    add restart button

--- app/javascript/packs/controllers/fight_controller.js
+++ app/javascript/packs/controllers/fight_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-   static targets = [ "hp" ]
+   static targets = [ "hp", 'restart']
   kick() {
     const hp = this.hpTarget.textContent
     if ( hp > 0) {
@@ -11,11 +11,20 @@ export default class extends Controller {
         this.hpTarget.classList.remove('text-success')
         this.hpTarget.classList.add('text-danger')
         this.hpTarget.innerHTML = 0
+        this.restartTarget.classList.remove('d-none')
         setTimeout(function(){ alert("You win !"); }, 0)
       } else {
         this.hpTarget.innerHTML = newHp
       }
     }
+  }
+  restart() {
+    const hp = this.hpTarget
+    hp.innerHTML = 100
+    this.restartTarget.classList.add('d-none')
+    this.hpTarget.classList.remove('text-danger')
+    this.hpTarget.classList.add('text-success')
+
   }
 }
 
