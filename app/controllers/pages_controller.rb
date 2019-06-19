3e514c87e6aa1bc384e8b00692947287f33d735b
Florent Legeron <legeronflo@hotmail.fr>
2019-06-19 11:56:27 +0200

    add map with modal

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,27 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :write_modal]
   def home
+    @users = User.where.not(latitude: nil, longitude: nil)
+
+    @markers = @users.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { user: user.id })
+      }
+    end
     @pizzas = MealCategory.pizzas
     @pastas = MealCategory.pastas
     @desserts = MealCategory.desserts
+  end
+
+  def write_modal
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+    end
+
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,15 @@ import "bootstrap";
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
+import { initMapbox } from '../plugins/init_mapbox';
 // Look for controllers inside app/javascripts/packs/controllers/
 const application = Application.start()
 const controllers = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(controllers))
 
+window.$ = window.jQuery = require('jquery');
+
+
+initMapbox();
